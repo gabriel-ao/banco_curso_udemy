f257d340db6e237be2da8e2c056f5ce45551a8f2
Gabriel Almeida de Oliveira <gabriel-ao@hotmail.com>
2018-03-05 15:29:39 -0300

    banco udemy poo
    trabalho em poo do curso udemy

---
+++ exercicio_Banco/exercicio_Banco/Conta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_Banco {
+    class Conta {
+
+        int numeroConta { get; private set; }
+        string nomeTitular { get; private set; }
+        double valorConta = 0;
+        int taxa = 5;
+
+        public Conta() {
+
+        }
+        public Conta(int numeroConta, string nomeTitular, double valorConta) {
+            this.numeroConta = numeroConta;
+            this.nomeTitular = nomeTitular;
+            this.valorConta = valorConta;
+        }
+
+        public double DepositandoValor(double novaEntrada) {
+            return valorConta += novaEntrada;
+        }
+
+        public double SacandoValor(double novaSaida) {
+            if (novaSaida < valorConta) {
+                return valorConta -= (taxa + novaSaida);
+            } else {
+                return valorConta;
+            }
+
+        }
+
+        public override string ToString() {
+            return " Conta: " + numeroConta
+                + " Titular: " + nomeTitular
+                + " Saldo: " + valorConta
+                ;
+        }
+    }
+}
---
+++ exercicio_Banco/exercicio_Banco/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_Banco {
+    class Program {
+        static void Main(string[] args) {
+
+            Conta conta;
+            double valorConta = 0;
+
+            Console.WriteLine("Digite o numero da conta: ");
+            int numeroConta = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o nome do titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial (s/n)?");
+            string opcao = Console.ReadLine();
+
+            if (opcao == "s") {
+                Console.WriteLine("Digite o valor do depósito inicial: ");
+                valorConta = Convert.ToDouble(Console.ReadLine());  
+            }
+
+            conta = new Conta(numeroConta, nome, valorConta);
+            Console.WriteLine("======================================");
+            Console.WriteLine(" Conta criada: " + conta.ToString());
+            Console.WriteLine("======================================");
+
+            Console.WriteLine("Digite um valor para depositar na conta");
+            double deposito = Convert.ToDouble(Console.ReadLine());
+            conta.DepositandoValor(deposito);
+
+            Console.WriteLine("======================================");
+            Console.WriteLine(" Conta atualizada: " + conta.ToString());
+            Console.WriteLine("======================================");
+
+
+            Console.WriteLine("Digite um valor para sacar da conta");
+            double saque = Convert.ToDouble(Console.ReadLine());
+            conta.SacandoValor(saque);
+
+            Console.WriteLine("======================================");
+            Console.WriteLine(" Conta atualizada: " + conta.ToString());
+            Console.WriteLine("======================================");
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
